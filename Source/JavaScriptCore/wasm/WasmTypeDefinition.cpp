6e366e7d58b7a14382daead5dafe2f92b9056d8e
Yusuke Suzuki <ysuzuki@apple.com>
2025-08-25 12:59:07 -0700

    ok

--- Source/JavaScriptCore/wasm/WasmTypeDefinition.cpp
+++ Source/JavaScriptCore/wasm/WasmTypeDefinition.cpp
@@ -611,56 +611,6 @@ bool TypeDefinition::isFinalType() const
     return true;
 }
 
-RTT::RTT(RTTKind kind)
-    : TrailingArrayType(1)
-    , m_kind(kind)
-    , m_displaySizeExcludingThis(size() - 1)
-{
-    at(0) = this;
-}
-
-RTT::RTT(RTTKind kind, const RTT& supertype)
-    : TrailingArrayType(supertype.size() + 1)
-    , m_kind(kind)
-    , m_displaySizeExcludingThis(size() - 1)
-{
-    ASSERT(supertype.size() == (supertype.displaySizeExcludingThis() + 1));
-    memcpySpan(span(), supertype.span());
-    at(supertype.size()) = this;
-}
-
-RefPtr<RTT> RTT::tryCreate(RTTKind kind)
-{
-    auto result = tryFastMalloc(allocationSize(/* itself */ 1));
-    void* memory = nullptr;
-    if (!result.getValue(memory))
-        return nullptr;
-    return adoptRef(new (NotNull, memory) RTT(kind));
-}
-
-RefPtr<RTT> RTT::tryCreate(RTTKind kind, const RTT& supertype)
-{
-    auto result = tryFastMalloc(allocationSize(supertype.size() + 1));
-    void* memory = nullptr;
-    if (!result.getValue(memory))
-        return nullptr;
-    return adoptRef(new (NotNull, memory) RTT(kind, supertype));
-}
-
-bool RTT::isSubRTT(const RTT& parent) const
-{
-    if (displaySizeExcludingThis() < parent.displaySizeExcludingThis())
-        return false;
-    return &parent == displayEntry(parent.displaySizeExcludingThis());
-}
-
-bool RTT::isStrictSubRTT(const RTT& parent) const
-{
-    if (displaySizeExcludingThis() <= parent.displaySizeExcludingThis())
-        return false;
-    return &parent == displayEntry(parent.displaySizeExcludingThis());
-}
-
 const FunctionSignature& TypeInformation::signatureForJSException()
 {
     return *singleton().m_Void_Externref;
