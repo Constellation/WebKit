1af7f10205c75804e75922455188efc460503582
Yusuke Suzuki <ysuzuki@apple.com>
2025-10-03 09:41:56 -0700

    ok

--- Source/JavaScriptCore/runtime/JSMicrotask.h
+++ Source/JavaScriptCore/runtime/JSMicrotask.h
@@ -32,6 +32,6 @@
 namespace JSC {
 
 void runJSMicrotask(JSGlobalObject*, MicrotaskIdentifier, JSValue job, std::span<const JSValue>);
-JSValue runInternalMirotask(JSGlobalObject*, MicrotaskIdentifier, InternalMicrotask, std::span<const JSValue> arguments);
+JS_EXPORT_PRIVATE JSValue runInternalMirotask(JSGlobalObject*, MicrotaskIdentifier, InternalMicrotask, std::span<const JSValue> arguments);
 
 } // namespace JSC
