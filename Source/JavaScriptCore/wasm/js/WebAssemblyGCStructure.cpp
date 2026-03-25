d13bff88b85ab8b5ca4b145fb79ec8e1d080726b
Yusuke Suzuki <ysuzuki@apple.com>
2026-03-25 01:16:24 -0700

    [JSC] Make WebAssemblyGCStructure realmless and rename Structure::globalObject() to realm()
    Make WebAssemblyGCStructure realmless by passing nullptr for globalObject,
    rename Structure::globalObject() to Structure::realm() and add
    JSObject::realmMayBeNull() for objects that may have a null realm.
    Add vm.wasmGCStructureMap to cache structures by RTT, and update all
    callers across JSC, WebCore, and WebKit.

--- Source/JavaScriptCore/wasm/js/WebAssemblyGCStructure.cpp
+++ Source/JavaScriptCore/wasm/js/WebAssemblyGCStructure.cpp
@@ -20,12 +20,13 @@
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
 #include "config.h"
 #include "WebAssemblyGCStructure.h"
 
+#include "DeferGC.h"
 #include "JSCInlines.h"
 #include "WasmFormat.h"
 
@@ -71,22 +72,27 @@ void WebAssemblyGCStructureTypeDependencies::process(Wasm::FieldType fieldType, 
     }
 }
 
-WebAssemblyGCStructure::WebAssemblyGCStructure(VM& vm, JSGlobalObject* globalObject, const TypeInfo& typeInfo, const ClassInfo* classInfo, Ref<const Wasm::TypeDefinition>&& unexpandedType, Ref<const Wasm::TypeDefinition>&& type, Ref<const Wasm::RTT>&& rtt)
-    : Structure(vm, StructureVariant::WebAssemblyGC, globalObject, typeInfo, classInfo)
+WebAssemblyGCStructure::WebAssemblyGCStructure(VM& vm, const TypeInfo& typeInfo, const ClassInfo* classInfo, Ref<const Wasm::TypeDefinition>&& unexpandedType, Ref<const Wasm::TypeDefinition>&& type, Ref<const Wasm::RTT>&& rtt)
+    : Structure(vm, StructureVariant::WebAssemblyGC, nullptr, typeInfo, classInfo)
     , m_rtt(WTF::move(rtt))
     , m_type(WTF::move(type))
     , m_typeDependencies(WebAssemblyGCStructureTypeDependencies { WTF::move(unexpandedType) })
 {
     setMayBePrototype(true);
+    ASSERT(!typeInfo.masqueradesAsUndefined()); // MasqueradesAsUndefined requires non-null Realm.
 }
 
-WebAssemblyGCStructure* WebAssemblyGCStructure::create(VM& vm, JSGlobalObject* globalObject, const TypeInfo& typeInfo, const ClassInfo* classInfo, Ref<const Wasm::TypeDefinition>&& unexpandedType, Ref<const Wasm::TypeDefinition>&& type, Ref<const Wasm::RTT>&& rtt)
+WebAssemblyGCStructure* WebAssemblyGCStructure::create(VM& vm, const TypeInfo& typeInfo, const ClassInfo* classInfo, Ref<const Wasm::TypeDefinition>&& unexpandedType, Ref<const Wasm::TypeDefinition>&& type, Ref<const Wasm::RTT>&& rtt)
 {
     ASSERT(vm.structureStructure);
-    WebAssemblyGCStructure* newStructure = new (NotNull, allocateCell<WebAssemblyGCStructure>(vm)) WebAssemblyGCStructure(vm, globalObject, typeInfo, classInfo, WTF::move(unexpandedType), WTF::move(type), WTF::move(rtt));
-    newStructure->finishCreation(vm);
-    ASSERT(newStructure->type() == StructureType);
-    return newStructure;
+    const Wasm::RTT* rttPtr = rtt.ptr();
+    DeferGC deferGC(vm);
+    return vm.wasmGCStructureMap.ensureValue(rttPtr, [&] {
+        WebAssemblyGCStructure* newStructure = new (NotNull, allocateCell<WebAssemblyGCStructure>(vm)) WebAssemblyGCStructure(vm, typeInfo, classInfo, WTF::move(unexpandedType), WTF::move(type), WTF::move(rtt));
+        newStructure->finishCreation(vm);
+        ASSERT(newStructure->type() == StructureType);
+        return newStructure;
+    });
 }
 
 } // namespace JSC
