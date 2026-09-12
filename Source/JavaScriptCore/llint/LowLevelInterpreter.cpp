6e3ce5b2dd8ac1eef652f65d2abe070156bfcbdb
Yusuke Suzuki <ysuzuki@apple.com>
2026-09-12 13:05:56 -0700

    [JSC] Tighten IPInt code https://bugs.webkit.org/show_bug.cgi?id=324059 rdar://187291986
    Reviewed by NOBODY (OOPS!).
    
    IPInt code is layout with 256B size for each handler. This patch applies
    many effort to reduce the size of this handler to make it within 128B size.
    Currently enabling this smaller size only for ARM64.
    
    1. Add ARM64 specific instructions to reduce # of instructions
    2. Define out-of-line code for very large handler and jump to that
       instead.
    
    This improves IPInt wasm execution by roughly ~5%, largely due to
    reduced size from 256B -> 128B and now IPInt main loop is much more tight.
    
    * JSTests/wasm/stress/ipint-atomic-cmpxchg-fast-path.js: Added.
    (async test):
    * Source/JavaScriptCore/llint/InPlaceInterpreter.asm:
    * Source/JavaScriptCore/llint/InPlaceInterpreter.cpp:
    * Source/JavaScriptCore/llint/InPlaceInterpreter.h:
    * Source/JavaScriptCore/llint/InPlaceInterpreter64.asm:
    * Source/JavaScriptCore/llint/LowLevelInterpreter.cpp:
    * Source/JavaScriptCore/offlineasm/arm64.rb:
    * Source/JavaScriptCore/offlineasm/instructions.rb:
    * Source/JavaScriptCore/offlineasm/risc.rb:
    * Source/JavaScriptCore/offlineasm/x86.rb:
    * Source/JavaScriptCore/wasm/WasmIPIntGenerator.h:

--- Source/JavaScriptCore/llint/LowLevelInterpreter.cpp
+++ Source/JavaScriptCore/llint/LowLevelInterpreter.cpp
@@ -547,8 +547,10 @@ WTF_ALLOW_UNSAFE_BUFFER_USAGE_END
 #define OFFLINE_ASM_NOALIGN ""
 
 #if CPU(ARM64) && OS(WINDOWS)
-// COFF uses power-of-two alignment: .align N means 2^N bytes
+// COFF uses power-of-two alignment: .align N means 2^N bytes. Every alignment an `aligned` label
+// can ask for needs an entry here, or the paste below names an undefined macro.
 #define OFFLINE_ASM_ALIGN_TRAP_256 "\n .align 8\n"
+#define OFFLINE_ASM_ALIGN_TRAP_128 "\n .align 7\n"
 #define OFFLINE_ASM_ALIGN_TRAP_64 "\n .align 6\n"
 #define OFFLINE_ASM_ALIGN_TRAP(align) OFFLINE_ASM_ALIGN_TRAP_##align
 #elif CPU(ARM64) || CPU(ARM64E)
